db2ed8c6b2549231fe4f03428f6d2f91535ea49e
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-01-29 16:38:18 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet >= 2500){
+    return "No can do."
+  }
+  else if (feet <= 400){
+   return "This one is on me!"
+  }
+  else if (feet > 400 && feet <=2000) {
+    return "That will be twenty bucks."
+  }
+  else if (feet > 2000){
+    return "I will gladly take your thirty bucks."
+  }
+  }
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : (city === "Pittsburgh" ? "No go." : "On my way!");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){ 
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+
+ 
 }
